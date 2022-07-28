9970c21653d9aa6581e99ef5992aaf33a9ce6655
Daniar <daniyaruteg123@gmail.com>
2022-07-28 16:23:44 +0500

    addSer

---
+++ user/serializer.py
@@ -0,0 +1,11 @@
+from asyncore import read, write
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    email = serializers.CharField()
+    password = serializers.CharField(write_only=True)
